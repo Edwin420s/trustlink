505aa7eed4ed1a331e43c6fbb1d742af22fb3239
Edwin Mwiti <eduedwyn5@gmail.com>
2025-11-09 13:37:33 +0300

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 import { WalletProvider } from './contexts/WalletContext'
+import { AppProvider } from './contexts/AppContext'
 import Navigation from './components/Navigation'
 import Landing from './pages/Landing'
 import Dashboard from './pages/Dashboard'
@@ -16,33 +17,35 @@ import Settings from './pages/Settings'
 function App() {
   return (
     <WalletProvider>
-      <Router>
-        <div className="min-h-screen bg-primary-50">
-          <Navigation />
-          <main>
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route path="/app" element={<Dashboard />} />
-              <Route path="/agreements" element={<Agreements />} />
-              <Route path="/record-proof" element={<RecordProof />} />
-              <Route path="/verify" element={<Verify />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
-          </main>
-          <ToastContainer
-            position="bottom-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-        </div>
-      </Router>
+      <AppProvider>
+        <Router>
+          <div className="min-h-screen bg-primary-50">
+            <Navigation />
+            <main>
+              <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/app" element={<Dashboard />} />
+                <Route path="/agreements" element={<Agreements />} />
+                <Route path="/record-proof" element={<RecordProof />} />
+                <Route path="/verify" element={<Verify />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/settings" element={<Settings />} />
+              </Routes>
+            </main>
+            <ToastContainer
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+          </div>
+        </Router>
+      </AppProvider>
     </WalletProvider>
   )
 }
